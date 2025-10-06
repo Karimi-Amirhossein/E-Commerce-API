05200d4a270bd8dcdcb083c995db5014bea20ee4
Amir_Karimi2002 <mr.amirhossein138181@gmail.com>
2025-10-06 14:30:00 +0200

    Feat: Create CustomUser model and reset database migrations

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """ Custom user model based on Django's AbstractUser. """
+
+    def __str__(self):
+        """Return a human-readable string representation of the user."""
+        return self.username
