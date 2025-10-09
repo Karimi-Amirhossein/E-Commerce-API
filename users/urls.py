7b542959f81174c86ea5645607ebde73aaad9cbd
Amir_Karimi2002 <mr.amirhossein138181@gmail.com>
2025-10-09 16:11:39 +0200

    feat: implement login and token generation API

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from .views import UserRegisterView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = "users" # Namespace for the users app
 
 urlpatterns = [ 
     path("register/", UserRegisterView.as_view(), name="register"),    # User registration endpoint
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # JWT login endpoint
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # JWT token refresh endpoint
+
 ]
